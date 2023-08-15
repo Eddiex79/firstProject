25c6b92fa9108d52d3f7d6a05cd252e6afb252a2
Adilson <Adilson@DESKTOP-OBRC5K5>
2023-08-15 12:41:43 -0300

    Primeiro Cimmit

---
+++ MeuPrimeiroPrograma/src/firstProject/meuPrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package firstProject;
+
+public class meuPrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Code created to send to github");
+
+	}
+
+}
---
+++ MeuPrimeiroPrograma/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Adilson
+ *
+ */
+module MeuPrimeiroPrograma {
+}
