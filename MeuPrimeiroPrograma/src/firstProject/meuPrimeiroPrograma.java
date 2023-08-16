a8f048f594c43079bc698bee8755d7e979ea1f10
Adilson <Adilson@DESKTOP-OBRC5K5>
2023-08-16 12:19:48 -0300

    revertido

--- MeuPrimeiroPrograma/src/firstProject/meuPrimeiroPrograma.java
+++ MeuPrimeiroPrograma/src/firstProject/meuPrimeiroPrograma.java
@@ -3,11 +3,8 @@ package firstProject;
 public class meuPrimeiroPrograma {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubjj
+		// TODO Auto-generated method stub
 		System.out.println("First Code created to send to github");
-		System.out.println("Teste de reverter commit");
-		//Apenas testando reversão de commit
-		
 
 	}
 
