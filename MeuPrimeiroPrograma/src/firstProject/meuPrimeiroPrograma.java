26bfc6649196c3c8c437ea1cef9a3518c873e633
Adilson <Adilson@DESKTOP-OBRC5K5>
2023-08-16 16:39:00 -0300

    if e else simple

--- MeuPrimeiroPrograma/src/firstProject/meuPrimeiroPrograma.java
+++ MeuPrimeiroPrograma/src/firstProject/meuPrimeiroPrograma.java
@@ -4,8 +4,12 @@ public class meuPrimeiroPrograma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("First Code created to send to github");
+		int age = 9;
+		if(age >= 18) {
+		System.out.println("Você eh maior de idade");
 
+	}else {
+		System.out.println("Vc eh menor de idade");
 	}
-
+	}
 }
